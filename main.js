71883cc88855888cc5b693f81e916f7609dc57b4
harivmasoor <harimasoor@gmail.com>
2023-07-19 12:59:18 -0700

    push

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import { initializePlayer, playSong } from './spotifyPlayer.js';
 document.getElementById('authorizeButton').addEventListener('click', function() {
     // Only redirect to Spotify's OAuth page.
     const clientId = "0cd96f761ce9434b9b4278b664d87591";
-    const redirectUri = encodeURIComponent("https://podify-two.vercel.app/"); 
+    const redirectUri = encodeURIComponent("https://harivmasoor.github.io/Podify/"); 
     const scopes = encodeURIComponent("user-top-read app-remote-control user-modify-playback-state");
 
     window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`;
@@ -18,7 +18,7 @@ const code = urlParams.get('code');
 
 if (code) {  
     // If there's an authorization code, send it to your backend for token exchange.
-    fetch("https://podify-two.vercel.app/spotify/token", {
+    fetch("https://harivmasoor.github.io/Podify/spotify/token", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',

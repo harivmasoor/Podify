2cba731312bac2a950edc7f6a4a56bad844283e6
harivmasoor <harimasoor@gmail.com>
2023-07-20 09:52:56 -0700

    push

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 import { initializePlayer, playSong } from './spotifyPlayer.js';
 
-// Rest of your code...
-
-
+// Constants
+const clientId = "0cd96f761ce9434b9b4278b664d87591";
+const redirectUri = encodeURIComponent("https://harivmasoor.github.io/Podify/"); 
+const scopes = encodeURIComponent("user-read-private user-top-read streaming");
 
 document.getElementById('authorizeButton').addEventListener('click', function() {
-    // Only redirect to Spotify's OAuth page.
-    const clientId ="0cd96f761ce9434b9b4278b664d87591"
-    const clientSecret ="ad546d7cc4534f2bb3af33d007a8e22a"
-    const redirectUri = encodeURIComponent("https://harivmasoor.github.io/Podify/"); 
-    const scopes = encodeURIComponent("user-read-private user-top-read streaming");
-
-
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`;
+    redirectToSpotifyOAuth();
 });
+
+function redirectToSpotifyOAuth() {
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`;
+}
 
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
 
 if (code) {  
-    // If there's an authorization code, send it to your backend for token exchange.
-    fetch("https://accounts.spotify.com/api/token", {
+    exchangeCodeForToken(code);
+}
+
+function exchangeCodeForToken(code) {
+    fetch("https://podify-backend.vercel.app/spotify/token", {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': 'Basic ' + btoa("0cd96f761ce9434b9b4278b664d87591" + ':' + "ad546d7cc4534f2bb3af33d007a8e22a")
+          'Content-Type': 'application/json'
         },
-        body: 'grant_type=client_credentials'
-      })
+        body: JSON.stringify({ code: code })
+    })
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -35,26 +35,26 @@ if (code) {
         return response.json();
     })
     .then(data => {
-        let token = data.access_token;
-        initializePlayer(token);
-                document.getElementById('authorizeButton').style.display = 'none';
-
-        // Show the player controls
-        document.getElementById('playerControls').style.display = 'block';
-        // ... Your logic here ...
+        initializeAppWithToken(data.access_token);
     })
     .catch(error => {
-        console.log("There was a problem with the fetch operation:", error.message);
+        console.error("Failed to exchange code for token:", error.message);
     });
 }
 
+function initializeAppWithToken(token) {
+    initializePlayer(token);
+    document.getElementById('authorizeButton').style.display = 'none';
+    document.getElementById('playerControls').style.display = 'block';
+}
+
 document.getElementById('playButton').addEventListener('click', function() {
-    // Replace with a valid Spotify URI for testing
-    let sampleTrackURI = 'spotify:track:6habFhsOp2NvshLv26DqMb';
-    playSong(sampleTrackURI);
+    playSong('spotify:track:6habFhsOp2NvshLv26DqMb');  // Replace with a valid Spotify URI for testing
 });
 
-document.getElementById('pauseButton').addEventListener('click', function() {
+document.getElementById('pauseButton').addEventListener('click', pauseSong);
+
+function pauseSong() {
     if (player) {
         player.pause().then(() => {
             console.log('Paused playback!');
@@ -62,17 +62,12 @@ document.getElementById('pauseButton').addEventListener('click', function() {
             console.error('Error trying to pause the playback:', error);
         });
     }
-});
+}
 
 window.onSpotifyWebPlaybackSDKReady = () => {
-    // This function will be called once the Spotify Web Playback SDK is ready.
-    // Since the Spotify player initialization requires the access token, which 
-    // you obtain after the OAuth flow, you do not need to immediately initialize 
-    // the player here. Instead, you're already doing that after receiving the token.
-
-    // For now, you can log a message indicating that the SDK is ready.
     console.log("Spotify Web Playback SDK is ready!");
 };
+
 
 
 
--- spotifyPlayer.js
+++ spotifyPlayer.js
@@ -10,27 +10,25 @@ function initializePlayer(token) {
         }
     });
     
-    // Error handling
+    setupPlayerListeners();
+    player.connect();
+}
+
+function setupPlayerListeners() {
     player.addListener('initialization_error', ({ message }) => { console.error(message); });
     player.addListener('authentication_error', ({ message }) => { console.error(message); });
     player.addListener('account_error', ({ message }) => { console.error(message); });
     player.addListener('playback_error', ({ message }) => { console.error(message); });
-    
-    // Playback status updates
+
     player.addListener('player_state_changed', state => { console.log(state); });
     
-    // Ready
     player.addListener('ready', ({ device_id }) => {
         console.log('Ready with Device ID', device_id);
     });
     
-    // Not Ready
     player.addListener('not_ready', ({ device_id }) => {
         console.log('Device ID has gone offline', device_id);
     });
-    
-    // Connect to the player
-    player.connect();
 }
 
 function playSong(uri) {
@@ -47,5 +45,17 @@ function playSong(uri) {
     });
 }
 
-export { initializePlayer, playSong };
+function pauseSong() {
+    if (!player) {
+        console.error("Player hasn't been initialized yet.");
+        return;
+    }
+    player.pause().then(() => {
+        console.log('Paused playback!');
+    }).catch(error => {
+        console.error('Error trying to pause the playback:', error);
+    });
+}
+
+export { initializePlayer, playSong, pauseSong };
 

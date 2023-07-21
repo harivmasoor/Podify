a88c9540808732afd1b6d19feef8c824c706e2ba
harivmasoor <harimasoor@gmail.com>
2023-07-21 16:33:05 -0700

    push

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ window.addEventListener('load', () => {
     getUserProfile(accessToken);
     setupSearch(accessToken);
     document.getElementById('searchInput').style.display = 'block'; // Show the search input
-    document.getElementById('searchResults').style.display = 'block';
+    // document.getElementById('searchResults').style.display = 'block';
   }
 });
 

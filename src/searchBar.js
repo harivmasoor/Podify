16fb65fafc140a77d1987d6c442a77659fd64f2a
harivmasoor <harimasoor@gmail.com>
2023-07-21 09:08:39 -0700

    push

--- src/searchBar.js
+++ src/searchBar.js
@@ -13,34 +13,54 @@ export function setupSearch(accessTokenValue) {
   });
 }
 
-function searchSpotify(query, accessToken) {
-    const searchEndpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=show,track&limit=5`;
-    const headers = {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-    };
+async function refreshToken() {
+  try {
+    const response = await fetch(`https://podify-backend.onrender.com/refresh_token?refresh_token=${localStorage.getItem('refresh_token')}`);
+    const data = await response.json();
+    accessToken = data.access_token;
+    localStorage.setItem('access_token', accessToken);
+  } catch (error) {
+    console.error('Error refreshing token:', error);
+  }
+}
 
-    return fetch(searchEndpoint, { headers: headers })
-        .then(response => response.json())
-        .then(data => {
-            const results = [];
-            if (data.shows && data.shows.items) {
-                results.push(...data.shows.items.map(item => ({
-                    type: 'show',
-                    name: item.name,
-                })));
-            }
-            if (data.tracks && data.tracks.items) {
-                results.push(...data.tracks.items.map(item => ({
-                    type: 'track',
-                    name: item.name,
-                })));
-            }
-            return results;
-        })
-        .catch(error => {
-            console.error('Error searching Spotify:', error);
-            return [];
-        });
+async function searchSpotify(query) {
+  const searchEndpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=show,track&limit=5`;
+  const headers = {
+    'Authorization': `Bearer ${accessToken}`,
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    let response = await fetch(searchEndpoint, { headers: headers });
+    
+    // If token expired (status 401), refresh and retry
+    if (response.status === 401) {
+      await refreshToken();
+      response = await fetch(searchEndpoint, { headers: headers });
+    }
+
+    const data = await response.json();
+    const results = [];
+    
+    if (data.shows && data.shows.items) {
+      results.push(...data.shows.items.map(item => ({
+        type: 'show',
+        name: item.name,
+      })));
+    }
+    
+    if (data.tracks && data.tracks.items) {
+      results.push(...data.tracks.items.map(item => ({
+        type: 'track',
+        name: item.name,
+      })));
+    }
+    
+    return results;
+  } catch (error) {
+    console.error('Error searching Spotify:', error);
+    return [];
+  }
 }
 

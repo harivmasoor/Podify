2a830be974da1234e3ab0822d894f3c486515dd8
harivmasoor <harimasoor@gmail.com>
2023-07-22 15:39:24 -0700

    push

--- src/index.js
+++ src/index.js
@@ -1,84 +1,96 @@
 import { setupSearch } from './searchBar.js';
 import { setupWebPlayer } from './webPlayer.js';
 
-// Define your Spotify API Client ID
-const clientId = '0cd96f761ce9434b9b4278b664d87591';
-
 const loginButton = document.getElementById('loginButton');  // Reference to the login button
 loginButton.addEventListener('click', () => {
-  window.location.href = 'https://podify-backend.onrender.com/login';
+    window.location.href = 'https://podify-backend.onrender.com/login';
 });
+
+let accessToken; // Move the declaration of accessToken to a higher scope
 
 window.addEventListener('load', () => {
-  const hash = window.location.hash.substring(1);
-  const hashParams = new URLSearchParams(hash);
+    const hash = window.location.hash.substring(1);
+    const hashParams = new URLSearchParams(hash);
 
-  if (hashParams.has('access_token')) {
-    loginButton.style.display = 'none';  // Hide the login button
+    if (hashParams.has('access_token')) {
+        loginButton.style.display = 'none';  // Hide the login button
 
-    const accessToken = hashParams.get('access_token');
-    const tokenType = hashParams.get('token_type');
-    const expiresIn = hashParams.get('expires_in');
-    const searchContainer = document.getElementById('search-container');
-    searchContainer.style.display = 'block';
+        accessToken = hashParams.get('access_token'); // Use the higher scoped accessToken
+        const tokenType = hashParams.get('token_type');
+        const expiresIn = hashParams.get('expires_in');
+        const searchContainer = document.getElementById('search-container');
+        searchContainer.style.display = 'block';
 
-    console.log({
-      access_token: accessToken,
-      token_type: tokenType,
-      expires_in: expiresIn,
-    });
+        console.log({
+            access_token: accessToken,
+            token_type: tokenType,
+            expires_in: expiresIn,
+        });
 
-    getUserProfile(accessToken);
-    setupSearch(accessToken);
-    const searchInput = document.getElementById('searchInput');
-    searchInput.style.display = 'block'; // Show the search input
-    
-    const searchResults = document.getElementById('searchResults');
-    searchInput.addEventListener('input', () => {
-      if (searchInput.value.length > 0) {
-          searchResults.style.display = 'block';  // Show the searchResults if there's content
-      } else {
-          searchResults.style.display = 'none';   // Hide the searchResults if there's no content
-      }})
-      setupWebPlayer(accessToken);    
-};
-})
-
-function getUserProfile(accessToken) {
-  const headers = {
-    Authorization: `Bearer ${accessToken}`,
-    'Content-Type': 'application/json',
-  };
-
-  fetch('https://api.spotify.com/v1/me', { headers: headers })
-    .then(response => response.json())
-    .then(data => {
-      const username = data.display_name;
-      renderUsername(username);
-    })
-    .catch(error => {
-      console.error('Error fetching user profile:', error);
-    });
-}
-
-player.addListener('player_state_changed', state => {
-  console.log(state);
-  isPlaying = !state.paused;
-  if (isPlaying) {
-      document.getElementById('playPause').textContent = '⏸️';
-  } else {
-      document.getElementById('playPause').textContent = '▶️';
-  }
+        getUserProfile(accessToken);
+        setupSearch(accessToken);
+        const searchInput = document.getElementById('searchInput');
+        searchInput.style.display = 'block'; // Show the search input
+        
+        const searchResults = document.getElementById('searchResults');
+        searchInput.addEventListener('input', () => {
+            if (searchInput.value.length > 0) {
+                searchResults.style.display = 'block';  // Show the searchResults if there's content
+            } else {
+                searchResults.style.display = 'none';   // Hide the searchResults if there's no content
+            }
+        });
+        setupWebPlayer(accessToken);    
+    }
 });
 
+function getUserProfile(token) {
+    const headers = {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+    };
+
+    fetch('https://api.spotify.com/v1/me', { headers: headers })
+        .then(response => response.json())
+        .then(data => {
+            const username = data.display_name;
+            renderUsername(username);
+        })
+        .catch(error => {
+            console.error('Error fetching user profile:', error);
+        });
+}
+
+var player;  // Declare the player variable at a scope accessible by all your functions
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+    player = new Spotify.Player({
+        name: 'Your Web Player Name',
+        getOAuthToken: callback => {
+            callback(accessToken);  // Use the higher scoped accessToken
+        }
+    });
+
+    player.addListener('player_state_changed', state => {
+        console.log(state);
+        isPlaying = !state.paused;
+        if (isPlaying) {
+            document.getElementById('playPause').textContent = '⏸️';
+        } else {
+            document.getElementById('playPause').textContent = '▶️';
+        }
+    });
+
+    player.connect();
+};
 
 function renderUsername(username) {
-  const usernameElement = document.getElementById('username');
-  usernameElement.textContent = username;
+    const usernameElement = document.getElementById('username');
+    usernameElement.textContent = username;
 }
 
 document.getElementById('searchInput').addEventListener('focus', (event) => {
-  event.preventDefault();
+    event.preventDefault();
 });
 
 document.getElementById('playPause').addEventListener('click', togglePlay);
@@ -92,14 +104,12 @@ function togglePlay() {
         player.pause().then(() => {
             console.log('Paused Playback');
             isPlaying = false;
-            // Update your playPause button's icon to "play" here
             document.getElementById('playPause').textContent = '▶️';
         });
     } else {
         player.resume().then(() => {
             console.log('Resumed Playback');
             isPlaying = true;
-            // Update your playPause button's icon to "pause" here
             document.getElementById('playPause').textContent = '⏸️';
         });
     }

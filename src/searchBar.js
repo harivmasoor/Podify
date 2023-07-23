d68a42063d1366733bec727d6cb17375bc2732ca
harivmasoor <harimasoor@gmail.com>
2023-07-22 22:29:48 -0700

    push

--- src/searchBar.js
+++ src/searchBar.js
@@ -6,11 +6,12 @@ export function setupSearch(accessTokenValue) {
   const searchInput = document.getElementById('searchInput');
   const searchResults = document.getElementById('searchResults');
   let timer;
+
   searchInput.addEventListener('input', (e) => {
     if (e.target.value) {
-      searchResults.style.border = '1px solid #ccc';  
+      searchResults.style.border = '1px solid #ccc';
     } else {
-      searchResults.style.border = 'none';  
+      searchResults.style.border = 'none';
     }
     clearTimeout(timer);
     timer = setTimeout(() => {
@@ -20,11 +21,18 @@ export function setupSearch(accessTokenValue) {
 }
 
 async function refreshToken() {
-  // ... (unchanged)
+  try {
+    const response = await fetch(`https://podify-backend.onrender.com/refresh_token?refresh_token=${localStorage.getItem('refresh_token')}`);
+    const data = await response.json();
+    accessToken = data.access_token;
+    localStorage.setItem('access_token', accessToken);
+  } catch (error) {
+    console.error('Error refreshing token:', error);
+  }
 }
 
 async function searchSpotify(query) {
-  const searchEndpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=album,artist,track,show,episode&limit=5`;
+  const searchEndpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=artist,show,episode,track&limit=5`;
   const headers = {
     'Authorization': `Bearer ${accessToken}`,
     'Content-Type': 'application/json',
@@ -33,6 +41,7 @@ async function searchSpotify(query) {
   try {
     let response = await fetch(searchEndpoint, { headers: headers });
     
+    // If token expired (status 401), refresh and retry
     if (response.status === 401) {
       await refreshToken();
       response = await fetch(searchEndpoint, { headers: headers });
@@ -41,56 +50,25 @@ async function searchSpotify(query) {
     const data = await response.json();
     const results = [];
     
-    // Extract and structure data for tracks
     if (data.tracks && data.tracks.items) {
       results.push(...data.tracks.items.map(item => ({
           type: 'track',
           id: item.id,
-          name: item.artists[0].name ? `${item.artists[0].name} - ${item.name}` : item.name,
-          image: item.album.images[0].url,
+          name: item.name, // Adjusted this line to include artist name
+          image: item.album.images[0].url, // Add image URL
       })));
-    }
-    
-    // Extract and structure data for episodes
-    if (data.episodes && data.episodes.items) {
-      results.push(...data.episodes.items.map(item => ({
-          type: 'episode',
-          id: item.id,
-          name: item.show ? `${item.show.name} - ${item.name}` : item.name,
-          image: item.images[0].url,
-      })));
-    }
+  }
+  
+  if (data.episodes && data.episodes.items) {
+    results.push(...data.episodes.items.map(item => ({
+        type: 'episode',
+        id: item.id,
+        name: item.name,  // Include show name
+        image: item.images[0].url,  // Add image URL
+    })));
+}
 
-    // Extract and structure data for artists
-    if (data.artists && data.artists.items) {
-      results.push(...data.artists.items.map(item => ({
-          type: 'artist',
-          id: item.id,
-          name: item.name,
-          image: item.images[0] ? item.images[0].url : null, // Ensure there is an image before accessing
-      })));
-    }
-
-    // Extract and structure data for albums
-    if (data.albums && data.albums.items) {
-      results.push(...data.albums.items.map(item => ({
-          type: 'album',
-          id: item.id,
-          name: item.name,
-          image: item.images[0].url,
-      })));
-    }
-    
-    // Extract and structure data for shows
-    if (data.shows && data.shows.items) {
-      results.push(...data.shows.items.map(item => ({
-          type: 'show',
-          id: item.id,
-          name: item.name,
-          image: item.images[0].url,
-      })));
-    }
-
+    // Call the displayResults function with the search results
     displayResults(results);
     
     return results;
@@ -107,15 +85,85 @@ function displayResults(results) {
   results.forEach(result => {
     const resultElement = document.createElement('div');
     resultElement.className = 'resultItem';
-    resultElement.dataset.id = result.id; 
+    resultElement.dataset.id = result.id;
     resultElement.dataset.type = result.type;
     resultElement.dataset.name = result.name;
     resultElement.dataset.image = result.image;
-    resultElement.innerHTML = `<img src="${result.image}" alt="${result.name}"/> ${result.name}`; // Display image and name
+    resultElement.innerHTML = `<img src="${result.image}" alt="${result.name}"/> ${result.name}`;
+    resultElement.addEventListener('click', handleResultClick);
     resultsContainer.appendChild(resultElement);
   });
 }
 
+async function handleResultClick(e) {
+  const type = e.currentTarget.dataset.type;
+  const id = e.currentTarget.dataset.id;
 
+  if (type === 'artist') {
+    await getArtistTopTracks(id);
+  } else if (type === 'show') {
+    await getShowEpisodes(id);
+  }
+}
+
+async function getArtistTopTracks(artistId) {
+  const topTracksEndpoint = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=US`;
+  const headers = {
+    'Authorization': `Bearer ${accessToken}`,
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    let response = await fetch(topTracksEndpoint, { headers: headers });
+
+    if (response.status === 401) {
+      await refreshToken();
+      response = await fetch(topTracksEndpoint, { headers: headers });
+    }
+
+    const data = await response.json();
+    const topTracks = data.tracks.map(track => ({
+      type: 'track',
+      id: track.id,
+      name: track.name,
+      image: track.album.images[0].url,
+    }));
+
+    displayResults(topTracks);
+
+  } catch (error) {
+    console.error('Error getting artist top tracks:', error);
+  }
+}
+
+async function getShowEpisodes(showId) {
+  const episodesEndpoint = `https://api.spotify.com/v1/shows/${showId}/episodes?market=US&limit=5`;
+  const headers = {
+    'Authorization': `Bearer ${accessToken}`,
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    let response = await fetch(episodesEndpoint, { headers: headers });
+
+    if (response.status === 401) {
+      await refreshToken();
+      response = await fetch(episodesEndpoint, { headers: headers });
+    }
+
+    const data = await response.json();
+    const episodes = data.items.map(episode => ({
+      type: 'episode',
+      id: episode.id,
+      name: episode.name,
+      image: episode.images[0].url,
+    }));
+
+    displayResults(episodes);
+
+  } catch (error) {
+    console.error('Error getting show episodes:', error);
+  }
+}
 
 

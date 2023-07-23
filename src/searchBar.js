bc0e560f3fde52cee04c47a2c4255dd6d0344206
harivmasoor <harimasoor@gmail.com>
2023-07-22 22:16:24 -0700

    push

--- src/searchBar.js
+++ src/searchBar.js
@@ -8,30 +8,23 @@ export function setupSearch(accessTokenValue) {
   let timer;
   searchInput.addEventListener('input', (e) => {
     if (e.target.value) {
-      searchResults.style.border = '1px solid #ccc';  // Add border
-  } else {
-      searchResults.style.border = 'none';  // Remove border
-  }
+      searchResults.style.border = '1px solid #ccc';  
+    } else {
+      searchResults.style.border = 'none';  
+    }
     clearTimeout(timer);
     timer = setTimeout(() => {
       searchSpotify(e.target.value);
-    }, 500);  // Half a second delay to reduce the number of requests
+    }, 500);
   });
 }
 
 async function refreshToken() {
-  try {
-    const response = await fetch(`https://podify-backend.onrender.com/refresh_token?refresh_token=${localStorage.getItem('refresh_token')}`);
-    const data = await response.json();
-    accessToken = data.access_token;
-    localStorage.setItem('access_token', accessToken);
-  } catch (error) {
-    console.error('Error refreshing token:', error);
-  }
+  // ... (unchanged)
 }
 
 async function searchSpotify(query) {
-  const searchEndpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=artist,show,episode,track&limit=5`;
+  const searchEndpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=album,artist,track,show,episode&limit=5`;
   const headers = {
     'Authorization': `Bearer ${accessToken}`,
     'Content-Type': 'application/json',
@@ -40,7 +33,6 @@ async function searchSpotify(query) {
   try {
     let response = await fetch(searchEndpoint, { headers: headers });
     
-    // If token expired (status 401), refresh and retry
     if (response.status === 401) {
       await refreshToken();
       response = await fetch(searchEndpoint, { headers: headers });
@@ -49,25 +41,56 @@ async function searchSpotify(query) {
     const data = await response.json();
     const results = [];
     
+    // Extract and structure data for tracks
     if (data.tracks && data.tracks.items) {
       results.push(...data.tracks.items.map(item => ({
           type: 'track',
           id: item.id,
-          name: item.artists[0].name ? item.artists[0].name + '-' + item.name : item.name, // Adjusted this line to include artist name
-          image: item.album.images[0].url, // Add image URL
+          name: item.artists[0].name ? `${item.artists[0].name} - ${item.name}` : item.name,
+          image: item.album.images[0].url,
       })));
-  }
-  
-  if (data.episodes && data.episodes.items) {
-    results.push(...data.episodes.items.map(item => ({
-        type: 'episode',
-        id: item.id,
-        name: item.show ? item.show.name + ' - ' + item.name : item.name,  // Include show name
-        image: item.images[0].url,  // Add image URL
-    })));
-}
+    }
+    
+    // Extract and structure data for episodes
+    if (data.episodes && data.episodes.items) {
+      results.push(...data.episodes.items.map(item => ({
+          type: 'episode',
+          id: item.id,
+          name: item.show ? `${item.show.name} - ${item.name}` : item.name,
+          image: item.images[0].url,
+      })));
+    }
 
-    // Call the displayResults function with the search results
+    // Extract and structure data for artists
+    if (data.artists && data.artists.items) {
+      results.push(...data.artists.items.map(item => ({
+          type: 'artist',
+          id: item.id,
+          name: item.name,
+          image: item.images[0] ? item.images[0].url : null, // Ensure there is an image before accessing
+      })));
+    }
+
+    // Extract and structure data for albums
+    if (data.albums && data.albums.items) {
+      results.push(...data.albums.items.map(item => ({
+          type: 'album',
+          id: item.id,
+          name: item.name,
+          image: item.images[0].url,
+      })));
+    }
+    
+    // Extract and structure data for shows
+    if (data.shows && data.shows.items) {
+      results.push(...data.shows.items.map(item => ({
+          type: 'show',
+          id: item.id,
+          name: item.name,
+          image: item.images[0].url,
+      })));
+    }
+
     displayResults(results);
     
     return results;
@@ -79,19 +102,20 @@ async function searchSpotify(query) {
 
 function displayResults(results) {
   const resultsContainer = document.getElementById('searchResults');
-  resultsContainer.innerHTML = '';  // Clear previous results
+  resultsContainer.innerHTML = '';
 
   results.forEach(result => {
     const resultElement = document.createElement('div');
     resultElement.className = 'resultItem';
     resultElement.dataset.id = result.id; 
     resultElement.dataset.type = result.type;
-    resultElement.dataset.name = result.name;  // Store the name for later retrieval
-    resultElement.dataset.image = result.image;  // Store the image URL for later retrieval
-    resultElement.innerHTML = result.name;
+    resultElement.dataset.name = result.name;
+    resultElement.dataset.image = result.image;
+    resultElement.innerHTML = `<img src="${result.image}" alt="${result.name}"/> ${result.name}`; // Display image and name
     resultsContainer.appendChild(resultElement);
-});
+  });
 }
+
 
 
 

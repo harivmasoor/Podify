67d13a8eaab29f8494b73e22750f4fc2d7a3ae9e
harivmasoor <harimasoor@gmail.com>
2023-07-22 21:15:21 -0700

    push

--- src/webPlayer.js
+++ src/webPlayer.js
@@ -22,7 +22,7 @@ async function playItem(itemId, itemType) {
     };
 
     try {
-        console.log("Attempting to play", constext_uri);
+        console.log("Attempting to play", context_uri);
         const response = await fetch(endpoint, {
             method: 'PUT',
             headers: headers,

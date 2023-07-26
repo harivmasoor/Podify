4d16ba6cbbe89ce3bf8c906d06a7c056a506ba75
harivmasoor <harimasoor@gmail.com>
2023-07-26 08:43:06 -0700

    push

--- src/audioCapture.js
+++ src/audioCapture.js
@@ -1,102 +1,4 @@
-const captureAudioButton = document.getElementById('captureAudio');
-let mediaRecorder;
-let audioChunks = [];
 
-const chunkDuration = 30000;
-const overlapDuration = 250;
-
-export function initializeAudioCapture() {
-    captureAudioButton.addEventListener('click', async () => {
-        if (!window.chrome || !chrome.tabCapture) {
-            console.error("chrome.tabCapture API is not available. Ensure the required extension is installed.");
-            return;
-        }
-
-        if (typeof mediaRecorder === 'undefined' || mediaRecorder.state === 'inactive') {
-            try {
-                chrome.tabCapture.capture({ audio: true, video: false }, function(stream) {
-                    if (chrome.runtime.lastError) {
-                        console.error('Error accessing the tab audio', chrome.runtime.lastError);
-                        return;
-                    }
-
-                    console.log("Acquired stream:", stream);
-                    console.log("Stream tracks:", stream.getTracks());
-
-                    mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
-
-                    mediaRecorder.ondataavailable = onDataAvailable;
-                    mediaRecorder.onstop = onRecordingStop;
-
-                    mediaRecorder.start(chunkDuration - overlapDuration);
-                    captureAudioButton.textContent = "Stop Recording";
-                });
-            } catch (err) {
-                console.error('Error accessing the tab audio', err);
-            }
-        } else if (mediaRecorder.state === 'recording') {
-            mediaRecorder.stop();
-            captureAudioButton.textContent = "Capture Sound";
-        }
-    });
-}
-
-function onDataAvailable(event) {
-    console.log("Data available event triggered", event);
-    if (event.data.size > 0) {
-        audioChunks.push(event.data);
-    }
-}
-
-function onRecordingStop() {
-    const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
-    downloadBlob(audioBlob, 'captured_audio.webm');
-    audioChunks = []; // Clear the array
-}
-
-function downloadBlob(blob, filename) {
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = filename;
-
-    document.body.appendChild(a);
-    a.click();
-
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-}
-
-// Rest of your code remains unchanged...
-
-async function sendToAPI(data) {
-    const formData = new FormData();
-    formData.append('audio', new Blob([data], { type: 'audio/webm' }), 'audio.webm');
-    try {
-        const response = await fetch('https://podify-backend.onrender.com/transcribe', {
-            method: 'POST',
-            body: formData
-        });
-
-        const responseData = await response.text();
-
-        let parsedData;
-        try {
-            parsedData = JSON.parse(responseData);
-        } catch (e) {
-            throw new Error(`Server responded with ${response.status}: ${responseData}`);
-        }
-
-        if (!response.ok) {
-            throw new Error(`Server responded with ${response.status}: ${parsedData.reason || responseData}`);
-        }
-
-        displayTranscription(parsedData);
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error.message);
-    }
-}
 
 function displayTranscription(result) {
     const transcriptionBox = document.getElementById('transcriptionBox');
@@ -111,6 +13,31 @@ function displayTranscription(result) {
 document.addEventListener('DOMContentLoaded', () => {
     initializeAudioCapture();
 });
+
+const socket = new WebSocket('ws://YOUR_BACKEND_URL/transcription');
+
+socket.onopen = function (event) {
+    console.log("WebSocket is open now.");
+    socket.send("Start Listening for transcription");  // You can send a message to start listening
+};
+
+socket.onmessage = function (event) {
+    console.log("Received data from server:", event.data);
+    // Here, you can handle the received transcription, e.g., display it in a textarea.
+};
+
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log(`Closed cleanly, code=${event.code}, reason=${event.reason}`);
+    } else {
+        console.log('Connection died');
+    }
+};
+
+socket.onerror = function (error) {
+    console.log(`Error: ${error.message}`);
+};
+
 
 
 
